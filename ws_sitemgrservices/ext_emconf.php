7234c1cc87cb43f3d4da5a2839bc52c32076ef14
boandersen <boandersen@735d13b6-9817-0410-8766-e36946ffe9aa>
2008-10-19 00:55:16 +0000

    import
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/ws_sitemgr/trunk@13127 735d13b6-9817-0410-8766-e36946ffe9aa

---
+++ ws_sitemgrservices/ext_emconf.php
@@ -0,0 +1,44 @@
+<?php
+
+########################################################################
+# Extension Manager/Repository config file for ext: "ws_sitemgrservices"
+#
+# Auto generated 19-10-2008 02:29
+#
+# Manual updates:
+# Only the data in the array - anything else is removed by next write.
+# "version" and "dependencies" must not be touched!
+########################################################################
+
+$EM_CONF[$_EXTKEY] = array(
+	'title' => 'Wildside Site manager services',
+	'description' => '',
+	'category' => 'services',
+	'author' => 'Bo Korshøj Andersen',
+	'author_email' => 'bo@wildside.dk',
+	'shy' => '',
+	'dependencies' => '',
+	'conflicts' => '',
+	'priority' => '',
+	'module' => '',
+	'state' => 'alpha',
+	'internal' => '',
+	'uploadfolder' => 0,
+	'createDirs' => '',
+	'modify_tables' => '',
+	'clearCacheOnLoad' => 0,
+	'lockType' => '',
+	'author_company' => '',
+	'version' => '0.0.0',
+	'constraints' => array(
+		'depends' => array(
+		),
+		'conflicts' => array(
+		),
+		'suggests' => array(
+		),
+	),
+	'_md5_values_when_last_written' => 'a:5:{s:9:"ChangeLog";s:4:"88ed";s:10:"README.txt";s:4:"ee2d";s:12:"ext_icon.gif";s:4:"1bdc";s:19:"doc/wizard_form.dat";s:4:"d213";s:20:"doc/wizard_form.html";s:4:"c226";}',
+);
+
+?>
